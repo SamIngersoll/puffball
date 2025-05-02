6038f496830f52d4dd1c1096d0f8502144fe4e1c
Ryan Reyes <ryan.david.reyes@gmail.com>
2025-05-02 14:08:20 -0400

    WIP spike
    Working on bug where it doesn't work on enemies.

--- levels/props/spike.gd
+++ levels/props/spike.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+@export var damage_dealt : float = 10
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	body.damage(10)
+	print("BODDYYYYYY", body)
+	body.damage(damage_dealt)
