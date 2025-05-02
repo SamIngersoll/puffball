96384287eb7c54c427dba4174ee0135a96f3ee9d
Sam Ingersoll <sam.ingersoll@gmail.com>
2025-05-02 16:56:54 -0400

    added the option for spikes to damage/not damage enemies.

--- levels/props/spike.gd
+++ levels/props/spike.gd
@@ -1,11 +1,11 @@
 extends Node3D
 
 @export var damage_dealt : float = 10
-
+@export var damage_enemies : bool = true
 func _on_area_3d_body_entered(body) -> void:
 	body.damage(damage_dealt)
 
 # semi-janky workaround using an area because the enemy wasnt working otherwise
 func _on_area_3d_area_entered(area: Area3D) -> void:
-	if area.owner.is_in_group("enemies"):
+	if area.owner.is_in_group("enemies") and damage_enemies:
 		area.owner.damage(damage_dealt)
